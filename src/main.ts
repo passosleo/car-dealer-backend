374234267a85040101f70b93b6a1b48035444688
passosleo <leonardopassos98@gmail.com>
2025-02-07 23:19:02 -0300

    chore: update libs

--- src/main.ts
+++ src/main.ts
@@ -5,20 +5,19 @@ import fastifyMultipart from '@fastify/multipart';
 import { sendResponse } from './infra/http/middlewares/response-sender-middleware';
 import { authRoutes } from './infra/http/routes/auth-routes';
 import fastifyRateLimit from '@fastify/rate-limit';
-import fastifyCors from '@fastify/cors';
 
 function main() {
   const app = Fastify();
 
   app.addHook('onRequest', sendResponse);
 
-  // CORS
-  app.register(fastifyCors, {
-    origin: ['https://car-dealer-frontend-eosin.vercel.app', 'https://car-dealer-backend-lake.vercel.app'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
-  });
+  // // CORS
+  // app.register(fastifyCors, {
+  //   origin: ['https://car-dealer-frontend-eosin.vercel.app', 'https://car-dealer-backend-lake.vercel.app'],
+  //   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  //   allowedHeaders: ['Content-Type', 'Authorization'],
+  //   credentials: true,
+  // });
 
   // Rate Limiting
   app.register(fastifyRateLimit, {
