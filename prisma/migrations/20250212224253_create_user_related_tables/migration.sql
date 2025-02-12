93347a3348b8bfdd5b39ab519993cd768616e5d4
passosleo <leonardopassos98@gmail.com>
2025-02-12 19:43:45 -0300

    fix: migrations

---
+++ prisma/migrations/20250212224253_create_user_related_tables/migration.sql
@@ -0,0 +1,35 @@
+-- CreateTable
+CREATE TABLE "tb_users" (
+    "user_id" TEXT NOT NULL,
+    "first_name" TEXT NOT NULL,
+    "last_name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "password_changed_at" TIMESTAMP(3),
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "created_by" TEXT NOT NULL,
+    "updated_at" TIMESTAMP(3),
+    "updated_by" TEXT,
+    "active" BOOLEAN NOT NULL DEFAULT true,
+
+    CONSTRAINT "tb_users_pkey" PRIMARY KEY ("user_id")
+);
+
+-- CreateTable
+CREATE TABLE "tb_users_password_reset_attempts" (
+    "attempt_id" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "attempt_count" INTEGER NOT NULL DEFAULT 0,
+    "last_attempt_at" TIMESTAMP(3) NOT NULL,
+    "blocked_until" TIMESTAMP(3),
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3),
+
+    CONSTRAINT "tb_users_password_reset_attempts_pkey" PRIMARY KEY ("attempt_id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "tb_users_email_key" ON "tb_users"("email");
+
+-- AddForeignKey
+ALTER TABLE "tb_users_password_reset_attempts" ADD CONSTRAINT "tb_users_password_reset_attempts_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "tb_users"("user_id") ON DELETE RESTRICT ON UPDATE CASCADE;
