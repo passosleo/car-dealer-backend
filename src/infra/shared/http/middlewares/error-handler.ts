c272bc0f95e08a12f3b57e471db3699a3987e8f0
passosleo <leonardopassos98@gmail.com>
2025-03-08 00:25:53 -0300

    fix: add prisma knwon request errors in error handler

--- src/infra/shared/http/middlewares/error-handler.ts
+++ src/infra/shared/http/middlewares/error-handler.ts
@@ -2,11 +2,22 @@ import { FastifyInstance } from 'fastify';
 import { hasZodFastifySchemaValidationErrors, isResponseSerializationError } from 'fastify-type-provider-zod';
 import { HttpException } from '../response/http-exception';
 import { HttpStatus } from '../response/http-status';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 export function setupErrorHandler(app: FastifyInstance) {
   app.setErrorHandler((err, req, res) => {
     if (err instanceof HttpException) {
       return res.sendResponse(err.statusCode, { error: err.message });
+    }
+
+    if (err instanceof PrismaClientKnownRequestError) {
+      if (err.code === 'P2003') {
+        return res.code(409).send({
+          statusCode: 409,
+          message: 'Conflict',
+          data: { error: 'Resource cannot be deleted because it is being used by another resource' },
+        });
+      }
     }
 
     if (hasZodFastifySchemaValidationErrors(err)) {
