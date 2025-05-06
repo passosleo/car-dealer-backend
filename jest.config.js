c0c6064291cb9f115f7a0265811dd86ab427dc8c
passosleo <leonardopassos98@gmail.com>
2025-05-06 20:19:58 -0300

    test: unit tests

--- jest.config.js
+++ jest.config.js
@@ -3,12 +3,12 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: [
-    '<rootDir>/src/application/use-cases/**/__tests__/**/*.spec.ts',
-    '<rootDir>/src/application/use-cases/**/__tests__/**/*.test.ts',
+    '<rootDir>/src/application/**/**/use-cases/__tests__/**/*.spec.ts',
+    '<rootDir>/src/application/**/**/use-cases/__tests__/**/*.test.ts',
   ],
   setupFilesAfterEnv: [],
   collectCoverageFrom: [
-    'src/application/use-cases/**/*.{ts,tsx}',
+    'src/application/**/**/use-cases/**/*.{ts,tsx}',
     '!src/**/*.d.ts',
     '!src/**/__tests__/**',
     '!src/**/*.spec.ts',
