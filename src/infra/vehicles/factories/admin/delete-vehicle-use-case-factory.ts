8dd7243147b03390bddfe4f7af6e3225007610b5
passosleo <leonardopassos98@gmail.com>
2025-05-15 00:15:02 -0300

    tests: unit tests banners

--- src/infra/vehicles/factories/admin/delete-vehicle-use-case-factory.ts
+++ src/infra/vehicles/factories/admin/delete-vehicle-use-case-factory.ts
@@ -1,9 +1,11 @@
 import { DeleteVehicleUseCase } from '../../../../application/vehicles/use-cases/admin/delete-vehicle-use-case';
+import { ImageStorageCloudinary } from '../../../shared/storages/image-storage-cloudinary';
 import { VehicleRepositoryPrisma } from '../../repositories/vehicle-repository-prisma';
 
 export class DeleteVehicleUseCaseFactory {
   public static create(): DeleteVehicleUseCase {
     const vehicleRepository = new VehicleRepositoryPrisma();
-    return new DeleteVehicleUseCase(vehicleRepository);
+    const imageStorage = new ImageStorageCloudinary();
+    return new DeleteVehicleUseCase(vehicleRepository, imageStorage);
   }
 }
